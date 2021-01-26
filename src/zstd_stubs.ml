25088b7e845d4954bcd023d94015827ce458501d
Joris Giovannangeli <joris.giovannangeli@ahrefs.com>
2021-01-26 08:24:18 +0100

    do not copy input buffer

--- src/zstd_stubs.ml
+++ src/zstd_stubs.ml
@@ -10,8 +10,8 @@ struct
   let getErrorName = foreign "ZSTD_getErrorName" (size_t @-> returning string)
   let isError = foreign "ZSTD_isError" (size_t @-> returning bool)
 
-  let do_compress = foreign "ZSTD_compress" (ptr void @-> size_t @-> string @-> size_t @-> int @-> returning size_t)
-  let do_decompress = foreign "ZSTD_decompress" (ptr void @-> size_t @-> string @-> size_t @-> returning size_t)
+  let do_compress = foreign "ZSTD_compress" (ptr void @-> size_t @-> ocaml_string @-> size_t @-> int @-> returning size_t)
+  let do_decompress = foreign "ZSTD_decompress" (ptr void @-> size_t @-> ocaml_string @-> size_t @-> returning size_t)
 
   let cctx : [`CCtx] structure typ = structure "ZSTD_CCtx_s"
   let create_cctx = foreign "ZSTD_createCCtx" (void @-> returning (ptr cctx))
