af608e8b9dffb0cd173d860c66905ba11590971a
Joris Giovannangeli <joris.giovannangeli@ahrefs.com>
2021-02-08 18:23:05 +0100

    fix undefined pointer arithmetic

--- src/zstd_ocaml_stubs.c
+++ src/zstd_ocaml_stubs.c
@@ -4,7 +4,7 @@
 size_t OCAMLZSTD_compress_offset(void *dst, size_t dstCapacity, const void *src,
                                  size_t srcOffset, size_t srcSize,
                                  int compressionLevel) {
-  return ZSTD_compress(dst, dstCapacity, src + srcOffset, srcSize,
+  return ZSTD_compress(dst, dstCapacity, (const char *)src + srcOffset, srcSize,
                        compressionLevel);
 }
 
@@ -12,7 +12,8 @@ size_t OCAMLZSTD_decompress_offset(void *dst, size_t dstCapacity,
                                    const void *src, size_t srcOffset,
                                    size_t compressedSize) {
 
-  return ZSTD_decompress(dst, dstCapacity, src + srcOffset, compressedSize);
+  return ZSTD_decompress(dst, dstCapacity, (const char *)src + srcOffset,
+                         compressedSize);
 }
 
 size_t OCAMLZSTD_compress_usingDict_offset(ZSTD_CCtx *ctx, void *dst,
@@ -21,8 +22,9 @@ size_t OCAMLZSTD_compress_usingDict_offset(ZSTD_CCtx *ctx, void *dst,
                                            const void *dict, size_t dictSize,
                                            int compressionLevel) {
 
-  return ZSTD_compress_usingDict(ctx, dst, dstCapacity, src + srcOffset,
-                                 srcSize, dict, dictSize, compressionLevel);
+  return ZSTD_compress_usingDict(ctx, dst, dstCapacity,
+                                 (const char *)src + srcOffset, srcSize, dict,
+                                 dictSize, compressionLevel);
 }
 
 size_t OCAMLZSTD_decompress_usingDict_offset(ZSTD_DCtx *dctx, void *dst,
@@ -30,6 +32,7 @@ size_t OCAMLZSTD_decompress_usingDict_offset(ZSTD_DCtx *dctx, void *dst,
                                              const void *src, size_t srcOffset,
                                              size_t srcSize, const void *dict,
                                              size_t dictSize) {
-  ZSTD_decompress_usingDict(dctx, dst, dstCapacity, src + srcOffset, srcSize,
-                            dict, dictSize);
+  ZSTD_decompress_usingDict(dctx, dst, dstCapacity,
+                            (const char *)src + srcOffset, srcSize, dict,
+                            dictSize);
 }
